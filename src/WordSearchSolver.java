0c3ba00cbf2a275e8d5d6d41d556bfc0792d4985
tastytypist <nathanael310802@gmail.com>
2022-01-25 02:29:08 +0700

    Update main program structure
    Changes:
    - Prefer file name input via CLI
    - Remove loop

--- src/WordSearchSolver.java
+++ src/WordSearchSolver.java
@@ -5,19 +5,11 @@ public class WordSearchSolver {
     static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        boolean exit = false;
-
         System.out.println("Welcome to Word Search Solver!\n");
-        while (!exit) {
-            WordSearch word_search = new WordSearch();
-            word_search.inputWordSearch(args[0]);
-            word_search.displayWordSearch();
-
-            System.out.println("Would you like to exit (y/n)?\040");
-            char choice = input.next().charAt(0);
-            if (choice == 'y') {
-                exit = true;
-            }
-        }
+        WordSearch word_search = new WordSearch();
+        word_search.inputWordSearch(args[0]);
+        word_search.displayWordSearch();
+        word_search.solveWordSearch();
+        word_search.displaySolutions();
     }
 }
